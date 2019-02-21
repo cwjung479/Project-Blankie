688f96326f1d4bbf1b4d8111158fe31af29165f8
natetrank <ntrank@calpoly.edu>
2019-02-21 14:43:37 -0800

    Base Level Select and Intro Menu
    Scenes, Sprites, Scripts. Just adding didn't change anything already in repo

---
+++ Blankie/Assets/LevelSelectScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectScript : MonoBehaviour
+{
+    public GameObject levels;
+    public GameObject options;
+
+    void Update()
+    {
+        if (options.active && Input.GetKeyDown(KeyCode.Escape))
+        {
+            options.SetActive(false);
+            levels.SetActive(true);
+        }
+    }
+
+    public void ChangeToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
